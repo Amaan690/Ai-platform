96df16dd432a7c484bd77268697aaf1b7792e772
Amaan690 <mdamanullah690@gmail.com>
2026-07-11 22:10:54 +0530

    Initial project setup with Tailwind CSS

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("max-h-0");
+  mobileMenu.classList.toggle("max-h-64");
+
+  mobileMenu.classList.toggle("opacity-0");
+  mobileMenu.classList.toggle("opacity-100");
+
+  // Button icon change
+  if (menuBtn.textContent.trim() === "☰") {
+    menuBtn.textContent = "✕";
+  } else {
+    menuBtn.textContent = "☰";
+  }
+});
